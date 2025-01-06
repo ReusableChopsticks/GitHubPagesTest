0726275de34aee862f57127da1bd0e4faba78672
ReusableChopsticks <gangstapotato36@gmail.com>
2025-01-06 19:22:22 +1100

    attempting to use hashrouter with github pages

---
+++ src/Page1.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+
+export default function Page1() {
+  return (
+    <div>
+      <p>Hello world</p>
+      <p>Hello world</p>
+      <p>Hello world</p>
+      <p>Hello world</p>
+      <p>Hello world</p>
+      <Link to="/">Back</Link>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router";
+import Page1 from './Page1.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path='page1' element={<Page1 />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
