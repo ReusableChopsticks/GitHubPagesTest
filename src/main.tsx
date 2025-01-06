ed53cea6f62c9a8124911e098aa8b4aaa4a058c0
ReusableChopsticks <gangstapotato36@gmail.com>
2025-01-06 19:24:28 +1100

    removed unused browserrouter import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import Page1 from './Page1.tsx';
 
 createRoot(document.getElementById('root')!).render(
